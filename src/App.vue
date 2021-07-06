0d94a7b814c1f7c58a0d1c476d145a9fb15cc5c5
LittleWhiteLoti <little.white.loti@gmail.com>
2021-07-05 23:55:35 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <h2>List without tracking and without key</h2>
+      <form @submit.prevent="addToUntrackedList">
+        <input type="text" name="untrackedName" :placeholder="untrackedList[0]" />
+        <button type="submit">Submit Name</button>
+      </form>
+      <ol>
+        <!-- eslint-disable vue/require-v-for-key -->
+        <li v-for="(li, i) in untrackedList">
+          <input type="text">
+          <button @click="deleteUntrackedItem(i)">Click to delete {{ li }}</button>
+        </li>
+      </ol>
+    </div>
+    <div>
+      <h2>List with tracking using :key</h2>
+      <form @submit.prevent="addToTrackedList">
+        <input type="text" name="trackedName" :placeholder="trackedList[0]" />
+        <button type="submit">Submit Name</button>
+      </form>
+      <ol>
+        <li v-for="(li, i) in trackedList" :key="li">
+          <input type="text">
+          <button @click="deleteTrackedItem(i)">Click to delete {{ li }}</button>
+        </li>
+      </ol>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+
+    let untrackedList = []
+
+    let trackedList = []
+
+    return {
+      untrackedList,
+      trackedList
+    }
+  },
+  methods: {
+    addToUntrackedList(e) {
+      this.untrackedList.push(e.srcElement[0].value)
+    },
+    addToTrackedList(e) {
+      this.trackedList.push(e.srcElement[0].value)
+    },
+    deleteUntrackedItem(i) {
+      console.log(i)
+      this.untrackedList.splice(i, 1)
+    },
+    deleteTrackedItem(i) {
+      this.trackedList.splice(i, 1)
+    } 
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+html, body {
+  margin: 0;
+  padding: 0
 }
+
+#app {
+  inset: 0
+}
+
 </style>
